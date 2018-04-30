e95abc601e0dd3699181ba623cb13bd2501f001a
hrick <henrique963@hotmail.com>
2018-04-29 23:56:15 -0300

    first commit

---
+++ Assets/Scripts/InimigoSpript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InimigoSpript : MonoBehaviour {
+    public float interval;
+    public float velocidadeMorte;
+    public float movespeed;
+    public float direcao;
+    public GameObject inimigosPrefab;
+    public Transform geradorPeixes;
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        Instantiate(inimigosPrefab,
+            geradorPeixes.transform.position,
+            geradorPeixes.transform.rotation);
+
+        yield return new WaitForSeconds(interval);
+        StartCoroutine(Start());
+    }
+
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+
+        print(c);
+        if (c.gameObject.tag == "Player")
+        {
+            float mover_y =   velocidadeMorte * Time.deltaTime;
+            transform.Translate(0.0f, mover_y, 0.0f);
+               
+            
+        }
+
+        if (c.gameObject.tag == "Chao")
+        {
+            Destroy(gameObject);
+        }
+
+        if (c.gameObject.tag == "Direita")
+        {
+            direcao = -1;
+        }
+        if (c.gameObject.tag == "Esquerda")
+        {
+            direcao = 1;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mover_x = direcao * movespeed * Time.deltaTime;
+        transform.Translate(mover_x, 0.0f, 0.0f);
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    Animator animator;
+
+    public float velocidade;
+	public float impulso;
+
+	public Transform chaoVerificador;
+	bool estaoNoChao;
+
+	SpriteRenderer spriteRender;
+	Rigidbody2D rb;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+        // Interface para os componentes
+        spriteRender = GetComponent<SpriteRenderer>();
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		
+		// Mover
+		float mover_x = Input.GetAxisRaw("Horizontal") * velocidade * Time.deltaTime;
+        print(Input.GetAxisRaw("Horizontal"));
+        transform.Translate (mover_x, 0.0f, 0.0f);
+
+		// Verificar colisao com o piso
+		estaoNoChao = Physics2D.Linecast(transform.position, 
+			chaoVerificador.position, 1 << LayerMask.NameToLayer("Piso"));
+
+
+        animator.SetFloat("run", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+
+        // Pulo
+        if (Input.GetButtonDown ("Jump") && estaoNoChao) {
+			rb.velocity = new Vector2 (0.0f, impulso);
+            animator.SetBool("pular", true);
+
+        } else
+        {
+            animator.SetBool("pular", false);
+        }
+		// Orientacao
+		if (mover_x > 0) {
+			spriteRender.flipX = true; 
+		} else if (mover_x < 0) {
+			spriteRender.flipX = false; 
+		}			
+
+
+	
+	}
+    void OnCollisionEnter2D(Collision2D c)
+    {
+
+        if (c.gameObject.tag == "PisoInimigo")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
